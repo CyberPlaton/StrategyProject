69225e6710a259f017899752a6cbd14e76102d19
Bogdan Strohonov <bogdan2006@web.de>
2022-07-06 10:35:18 +0200

    Add: Fix critical Bug in CEntityStatusEffectMngr::Update function.

--- StrategyClient/src/Status.h
+++ StrategyClient/src/Status.h
@@ -13,6 +13,16 @@ namespace cherrysoda
 	class EntityStatusEffect
 	{
 	public:
+		enum ETimerType
+		{
+			TT_TURN,			// Status Effect is applied for a number of turns. TimerValue defines for how many turns the SE stays applied.
+			
+			TT_PERSISTENT,		// Status Effect is persistent until it is removed. TimerValue is ignored.
+
+			TT_REOCCURRING,		// Status Effect is persistent until it is removed, but after the removal condition is not more valid it is re-applied. 
+								// TimerValue defines how many turns the condition has to be invalid, in order to re-apply the SE again.
+		};
+	public:
 		/// @brief Pointer to the entity this effect is applied to.
 		/// @return Entity Pointer.
 		virtual Entity* Self() = 0;
@@ -97,29 +107,36 @@ namespace cherrysoda
 		void Update()
 		{
 			std::vector< EntityStatusEffect* > remaining_effects;
+			std::vector< EntityStatusEffect* > removing_effects;
 
-			// Execute and store which should remain.
+			// Execute and store which should remain and which should be removed.
 			for(int i = 0; i < m_statusEffectsVec.size(); i++)
 			{
 				auto& effect = m_statusEffectsVec[i];
 
-				if(effect->OnUpdate() == false)
+				if(effect->OnUpdate() == true)
 				{
-					remaining_effects.push_back(m_statusEffectsVec[i]);
+					remaining_effects.push_back(m_statusEffectsVec[i]); // Remain.
+				}
+				else
+				{
+					removing_effects.push_back(m_statusEffectsVec[i]); // Remove.
 				}
 			}
 
-			// Remove all Status Effects and re-append remaining.
-			while(m_statusEffectsVec.size() > 0)
-			{
-				auto effect = m_statusEffectsVec[0];
-				delete effect;
-				m_statusEffectsVec.erase(m_statusEffectsVec.begin());
-			}
-
+			// Remove all Status Effects (But do not delete them!) and re-append remaining.
+			m_statusEffectsVec.clear();
 			for(int i = 0; i < remaining_effects.size(); i++)
 			{
 				m_statusEffectsVec.push_back(remaining_effects[i]);
+			}
+
+			// Delete the unneeded effects.
+			while(removing_effects.size() > 0)
+			{
+				auto effect = removing_effects[0];
+				delete effect; effect = nullptr;
+				removing_effects.erase(removing_effects.begin());
 			}
 		}
 
