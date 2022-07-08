73c8271cfdc8d0a536b0edfbac5f4a94078be461
Bogdan Strohonov <bogdan2006@web.de>
2022-07-08 23:10:40 +0200

    Add: Behavior Tree Limit Node.

---
+++ BehaviorTree/src/BTLimit.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "BTDecorator.h"
+
+namespace bt
+{
+	/// @brief Inverter is a flow control Behavior Tree Node. It runs its Tick only if the limit has not been reached.
+	class BTLimit : public BTDecorator
+	{
+	public:
+		BTLimit(std::string name, size_t limit) : BTDecorator(name), m_Limit(limit)
+		{
+		}
+		virtual ~BTLimit()
+		{
+		}
+
+
+		//////////////////////////////
+		// Limit Base functionality.
+		//////////////////////////////
+		EBTNodeResult Tick() override final
+		{
+			if(m_Runs <= m_Limit)
+			{
+				EBTNodeResult result = m_Child->Tick();
+
+				m_Runs++;
+				return result;
+			}
+			else
+			{
+				return EBTNodeResult::FAILURE;
+			}
+		}
+		std::string Type() override
+		{
+			return "BTLimit";
+		}
+	protected:
+		size_t m_Runs = 0;
+		size_t m_Limit = 0;
+	};
+}
